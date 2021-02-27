cc9545815d4e3de90afe627b7405135e6f1ee1da
sb4255 <57270552+sb4255@users.noreply.github.com>
2021-02-27 01:52:18 -0500

    Initial commit

--- Flashcards3/ViewController.swift
+++ Flashcards3/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answer_back: UILabel!
+    
+    @IBOutlet weak var question_front: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapOnFlashCard(_ sender: Any) {
+        if(question_front.isHidden == false){
+            question_front.isHidden = true
+            
+        }
+        else {
+            question_front.isHidden = false
+            
+        }
+        print("mmmm")
+    }
+    
+    @IBAction func didTapAnswer(_ sender: Any) {
+        answer_back.isHidden = true
+    }
 }
 
