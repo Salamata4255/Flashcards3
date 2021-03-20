d390415edf78f5bb22a0c488ea22fe548c950b49
sb4255 <57270552+sb4255@users.noreply.github.com>
2021-03-20 02:56:15 -0400

    new commit

--- Flashcards3/ViewController.swift
+++ Flashcards3/ViewController.swift
@@ -7,14 +7,38 @@
 
 import UIKit
 
+struct Flashcard{
+    var question: String
+    var answer: String
+}
 class ViewController: UIViewController {
 
     @IBOutlet weak var answer_back: UILabel!
     
     @IBOutlet weak var question_front: UILabel!
+    
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
+    
+    @IBAction func didTapOnPrev(_ sender: Any) {
+    }
+    
+    
+    @IBAction func didTapOnNext(_ sender: Any) {
+        currentIndex = currentIndex + 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateFlashcard(question: "what month is this?", answer: "March")
     }
 
     @IBAction func didTapOnFlashCard(_ sender: Any) {
@@ -26,7 +50,6 @@ class ViewController: UIViewController {
             question_front.isHidden = false
             
         }
-        print("mmmm")
     }
     
     @IBAction func didTapAnswer(_ sender: Any) {
@@ -34,15 +57,46 @@ class ViewController: UIViewController {
     }
     
     func updateFlashcard(question: String, answer: String) {
-        question_front.text = question
-        answer_back.text = answer
+        let flashcard = Flashcard(question: question, answer: answer)
+        flashcards.append(flashcard)
         
+        print("Added new")
+        print("we now have \(flashcards.count) flashcards")
+        
+        currentIndex = flashcards.count - 1
+        print("our curr index is \(currentIndex)")
+        
+        updateNextPrevButtons()
+        updateLabels()
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navigationController = segue.destination as! UINavigationController
         let createController = navigationController.topViewController as! CreationViewController
         createController.flashcardsController = self
         
+    }
+    
+    func updateNextPrevButtons(){
+        
+        if currentIndex == 0 {
+            prevButton.isEnabled = false
+        }else {
+            prevButton.isEnabled = true
+        }
+        if currentIndex == flashcards.count - 1{
+            nextButton.isEnabled = false
+        } else{
+            nextButton.isEnabled = true
+        }
+        
+       
+    }
+    
+    func updateLabels() {
+        let currentFlashcard = flashcards[currentIndex]
+        
+        question_front.text = currentFlashcard.question
+        answer_back.text = currentFlashcard.answer
     }
     
 }
